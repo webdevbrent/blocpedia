379ba00ac932e409fbd3e84f72c4da7119342329
webdevbrent <webdeveloperbrent@gmail.com>
2018-09-12 13:50:39 -0400

    Protected private views for authorized users. Downgraded users now sets private wikis to public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
+
   def downgrade_account
-    current_user.standard!
+    user = current_user
+    user.standard!
+    user.wikis.update(private: false)
     flash[:notice] = "Your account has been downgraded."
     redirect_to edit_user_registration_path
   end
+
+
 end
